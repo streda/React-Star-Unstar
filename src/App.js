758d61fd2c9ca4766dbba7ea91ca3b1b05565902
streda <55853850+streda@users.noreply.github.com>
2023-10-27 16:50:59 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { initialLetters } from "./data.js";
+import Letter from "./Letter.js";
+
+export default function MailClient() {
+  const [letters, setLetters] = useState(initialLetters);
+  const [highlightedId, setHighlightedId] = useState(null);
+
+  function handleHover(letterId) {
+    setHighlightedId(letterId);
+  }
+
+  function handleStar(starredId) {
+    setLetters(
+      letters.map((letter) => {
+        if (letter.id === starredId) {
+          return {
+            ...letter,
+            isStarred: !letter.isStarred
+          };
+        } else {
+          return letter;
+        }
+      })
+    );
+    setHighlightedId(starredId);
+  }
+
+  return (
+    <>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map((letter) => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isHighlighted={letter.id === highlightedId}
+            onHover={handleHover}
+            onToggleStar={handleStar}
+          />
+        ))}
+      </ul>
+    </>
+  );
+}
---
+++ src/Letter.js
@@ -0,0 +1,53 @@
+// export default function Letter({
+//   letter,
+//   isHighlighted,
+//   onHover,
+//   onToggleStar
+// }) {
+//   return (
+//     <li
+//       className={isHighlighted ? "highlighted" : ""}
+//       onFocus={() => {
+//         onHover(letter.id);
+//       }}
+//       onPointerMove={() => {
+//         onHover(letter.id);
+//       }}
+//     >
+//       <button
+//         onClick={() => {
+//           onToggleStar(letter.id);
+//         }}
+//       >
+//         {letter.isStarred ? "Unstar" : "Star"}
+//       </button>
+//       {letter.subject}
+//     </li>
+//   );
+// }
+
+export default function Letter({
+  letter,
+  isHighlighted,
+  onHover,
+  onToggleStar
+}) {
+  return (
+    <li
+      className={isHighlighted ? "highlighted" : ""}
+      onFocus={() => {
+        onHover(letter.id);
+      }}
+    >
+      <button
+        className={isHighlighted ? "highlighted" : ""}
+        onClick={() => {
+          onToggleStar(letter.id);
+        }}
+      >
+        {letter.isStarred ? "Unstar" : "Star"}
+      </button>
+      {letter.subject}
+    </li>
+  );
+}
